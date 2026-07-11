b5d37c18bab45e7a25ef828499f113aa7c11445f
patit <patitroy29@gmail.com>
2026-07-12 00:07:22 +0530

    Add GitHub Pages deployment build

--- dist/_next/static/OIbgm4tcQYEgvSzF6-HyH/_buildManifest.js
+++ dist/_next/static/OIbgm4tcQYEgvSzF6-HyH/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/weeding//_next/:path+",
+        "destination": "/weeding/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
